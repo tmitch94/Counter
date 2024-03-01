2f4a0a8031073812e5a3b368c4846de4f3608050
tmitch94 <tacia.mitchell@icloud.com>
2024-03-01 11:52:47 -0500

    Made buttons have one class and changed color. added transitions and hover animation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let total = document.getElementById("total").textContent = 0;
 total = Number(total);
 
 
-document.getElementById("b1").onclick = function(){
+document.getElementById("increase").onclick = function(){
     total+=1
     document.getElementById("total").textContent = total;
     console.log(total);
@@ -13,7 +13,7 @@ document.getElementById("b2").onclick = function(){
     document.getElementById("total").textContent = total;
 }
 
-document.getElementById("b3").onclick = function(){
+document.getElementById("decrease").onclick = function(){
     total-=1
     document.getElementById("total").textContent = total;
 
